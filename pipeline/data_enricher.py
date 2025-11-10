9ef8bb996c6f8ae1c1095ca9861d32e1b0398d36
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-11-10 23:34:34 +0100

    pd.DataFrame use for review

--- pipeline/data_enricher.py
+++ pipeline/data_enricher.py
@@ -1,36 +1,24 @@
 import pandas as pd
+import requests
+import json
+from pandas import json_normalize
 
 
-class DataEnricher:
-    def __init__(self, products: list[dict], users: list[dict]):
-        self.products_raw = products
-        self.users_raw = users
 
-        # Convert to DataFrames
-        self.products_df = pd.DataFrame(products)
-        self.users_df = pd.DataFrame(users)
+api_url_product = 'https://fakestoreapi.com/products'
+api_url_users = 'https://fakestoreapi.com/products'
 
-    def enrich(self):
-        # Merge products with user data
-        enriched_df = self.products_df.merge(
-            self.users_df,
-            how="left",
-            left_on="seller_id",   # or whatever your key is
-            right_on="user_id",
-            suffixes=("", "_user"),
-        )
+response_product = requests.get(api_url_product)
+response_users = requests.get(api_url_users)
 
-        # Compute revenue inside enrich()
-        def compute_revenue(row):
-            rating = row.get("rating", {})
-            rating_count = rating.get("count", 0) if isinstance(rating, dict) else 0
-            price = row.get("price", 0)
-            return price * rating_count
+product = response_product.json()
+users = response_users.json()
 
-        enriched_df["revenue"] = enriched_df.apply(compute_revenue, axis=1)
-        return enriched_df
+df_read = pd.DataFrame(product)
+print(df_read)
+print(df_read.head())
 
-    def to_csv(self, path: str):
-        enriched_df = self.enrich()
-        enriched_df.to_csv(path, index=False)
-        print(f"Enriched data saved to {path}")
+
+df_read_users = pd.DataFrame(users)
+print(df_read_users)
+print(df_read_users.head())
