edb0ed11307b0d0d7fb40ca5a103282e072937f5
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-11-10 23:52:30 +0100

    pd.DataFrame get and clean request

--- pipeline/data_enricher.py
+++ pipeline/data_enricher.py
@@ -1,24 +1,22 @@
 import pandas as pd
 import requests
-import json
-from pandas import json_normalize
 
+api_url_products = 'https://fakestoreapi.com/products'
+api_url_users = 'https://fakestoreapi.com/users'
 
-
-api_url_product = 'https://fakestoreapi.com/products'
-api_url_users = 'https://fakestoreapi.com/products'
-
-response_product = requests.get(api_url_product)
+response_products = requests.get(api_url_products)
 response_users = requests.get(api_url_users)
 
-product = response_product.json()
+# Ensure HTTP requests succeeded (will raise HTTPError on bad status)
+response_products.raise_for_status()
+response_users.raise_for_status()
+
+# Call the .json() method to get parsed JSON data (not the method object)
+products = response_products.json()
 users = response_users.json()
 
-df_read = pd.DataFrame(product)
-print(df_read)
-print(df_read.head())
-
-
+df_read_products = pd.DataFrame(products)
 df_read_users = pd.DataFrame(users)
-print(df_read_users)
+
+print(df_read_products.head())
 print(df_read_users.head())
